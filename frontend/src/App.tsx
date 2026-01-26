d9ff2702565e6e467b277141bca26cc427680c67
unknown <abdurrahmanaikon@gmail.com>
2026-01-26 09:45:40 +0700

    penambahan

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 // frontend/src/App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/dashboard/dashboard";
+import Dashboard from "./pages/dashboard/landingPage";
 import ContentGeneration from "./pages/content-generation/ContentGeneration";
 import TrendsGeneration from "./pages/trends-generation/TrendsGeneration";
+import MarketingDashboard from "./pages/marketing-dashboard/MarketingDashboard";
+import ComingSoonPage from "./pages/ComingSoonPage";
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
           <Route path="/generator" element={<ContentGeneration />} />
           <Route path="/trends" element={<TrendsGeneration />} />
+          <Route path="/ComingSoonPage" element={<ComingSoonPage title={""} desc={""} />} />
+          <Route path="/marketing-dashboard" element={<MarketingDashboard />} />
           
       </Routes>
     </BrowserRouter>
